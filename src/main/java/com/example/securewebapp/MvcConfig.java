2c9f11bc8a3c2aa4a26352bb8019890aa091885c
ldierk <ldierk@intime.de>
2021-03-23 15:37:34 +0100

    .

--- src/main/java/com/example/securewebapp/MvcConfig.java
+++ src/main/java/com/example/securewebapp/MvcConfig.java
@@ -7,12 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/home", "home");
-        registry.addRedirectViewController("/", "home");
-        registry.addRedirectViewController("/hello", "hello");
-        registry.addRedirectViewController("/login", "login");
-    }
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+	}
 
 }
 
--- src/main/java/com/example/securewebapp/WebSecurityConfig.java
+++ src/main/java/com/example/securewebapp/WebSecurityConfig.java
@@ -13,11 +13,10 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().logout()
-                .permitAll();
+        http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
+                .loginPage("/login").permitAll().and().logout().permitAll();
     }
 
     @Bean
@@ -25,6 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
                 .build();
+
         return new InMemoryUserDetailsManager(user);
     }
 }
@@ -33,6 +33,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
  * 
  * 
  * Ensures that only authenticated users can see the secret greeting:
+ * 
+ * 
+ * The WebSecurityConfig class is annotated with @EnableWebSecurity to enable
+ * Spring Security’s web security support and provide the Spring MVC
+ * integration. It also extends WebSecurityConfigurerAdapter and overrides a
+ * couple of its methods to set some specifics of the web security
+ * configuration.
+ * 
+ * The configure(HttpSecurity) method defines which URL paths should be secured
+ * and which should not. Specifically, the / and /home paths are configured to
+ * not require any authentication. All other paths must be authenticated.
+ * 
+ * When a user successfully logs in, they are redirected to the previously
+ * requested page that required authentication. There is a custom /login page
+ * (which is specified by loginPage()), and everyone is allowed to view it.
+ * 
+ * The userDetailsService() method sets up an in-memory user store with a single
+ * user. That user is given a user name of user, a password of password, and a
+ * role of USER.
  * 
  * 
  */
